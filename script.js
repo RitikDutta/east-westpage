c80caeb350667e246ed91b4818a41acd937e1d7d
ritik dutta <ritikduttagd@gmail.com>
2023-10-10 14:55:31 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    if (name === "" || email === "" || message === "") {
+        alert("Please fill out all fields.");
+        event.preventDefault();
+    }
+});
